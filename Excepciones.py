d2ed814fa5dde85373bfc3bb250fa8ee6bb825db
oscarfernandez01 <oscar.fernandez@beecker.ai>
2024-08-12 16:52:49 -0600

    Agregación de color

--- Excepciones.py
+++ Excepciones.py
@@ -1,1 +1,16 @@
+# Un ejemplo de error
+# print(1/0) 0-9
+
+numero_var = "hola"
+
+try:
+    print(int(numero_var))
+except Exception as error :
+    print(f" El valor que se recibe no es tipo entero {error}")
+
 print("Hola Mundo")
+
+def ejemplo():
+    return
+
+ejemplo()
---
+++ POO.py
@@ -0,0 +1,16 @@
+class Automovil:
+    def tipo_color(self,inColor,inRed,inBlue,inGreen):
+        print(f"El color del auto es: {inColor}")
+        print(f"El color del auto es: {inRed}")
+        print(f"El color del auto es: {inBlue}")
+        print(f"El color del auto es: {inGreen}")
+
+
+
+Spark = Automovil()
+Aveo = Automovil()
+Bmw = Automovil()
+
+Spark.tipo_color(inColor="Blue",inBlue="255",inGreen="0",inRed="0")
+Aveo.tipo_color(inColor="Blue",inBlue="255",inGreen="0",inRed="0")
+Bmw.tipo_color(inColor="Blue",inBlue="255",inGreen="0",inRed="0")
